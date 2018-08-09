7a8ca6bc9a31935c972bbb831dbc8350b2d60a69
Imoa1991 <ordonez.im@gmail.com>
2018-08-09 08:56:11 +0200

    terminado el scss de Character

--- src/components/Character.js
+++ src/components/Character.js
@@ -23,11 +23,14 @@ class Character extends React.Component {
       <ul className="card_container">
         <li className="card_list" key={this.character.id}>
           <img src={this.character.image} alt="poster"  className="card__poster"/>
-          <h2 className="card__name">{this.character.name}</h2>
-          <p className="card__house">Casa: {this.character.house}</p>
-          <p className="card__birthday">Brithday: {this.character.dateOfBirth}</p>
-          {this.getAlive(this.character.alive)}
-          <p className="card__patronus">{this.character.patronus}</p>
+          <div className="card_details">
+            <h2 className="card__name">{this.character.name}</h2>
+            <p className="card__house">Casa: {this.character.house}</p>
+            <p className="card__birthday">Brithday: {this.character.dateOfBirth}</p>
+            <p className="card__patronus">Patronus: {this.character.patronus}</p>
+            {this.getAlive(this.character.alive)}
+
+          </div>
         </li>
       </ul>
     </React.Fragment>
